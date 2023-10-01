b6dc7b87989d88cb06b4122f1955f19606729cd2
Jentrix <jentrix.ilusa@student.moringaschool.com>
2023-10-01 12:27:02 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+console.log('After DOM Loaded');
+console.log(document.querySelector('div'))
+let form=document.querySelector('#create-task-form');
+form.addEventListener('submit',(e)=>{
+  e.preventDefault();
+  buildToDo(e.target.new-task-description.value);
+  form.requestFullscreen();
 });
+});
+
+function buildToDo(todo){
+  let ul=document.createElement('ul');
+  let btn=document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent='x'
+  ul.textContent=`${todo}`;
+  ul.appendChild(btn);
+  console.log(ul);
+  document.querySelector('#list').appendChild(ul);
+}
+function handleDelete(e){
+  e.target.parentNode.remove();
+}
